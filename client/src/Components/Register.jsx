1a9afa5f78355007ae4719c51c187d5d3685e36a
pranita1999tayde <144302372+pranita1999tayde@users.noreply.github.com>
2023-11-22 07:22:44 +0530

    update

--- client/src/Components/Register.jsx
+++ client/src/Components/Register.jsx
@@ -7,22 +7,48 @@ function Register(){
         setUserdata({...userData,[event.target.name]:event.target.value })
 
     }
+    const handleSubmit = async(event) => {
+        event.preventDefault();
+        if(userData.name && userData.email && userData.password){
+
+            // try{
+            //     const response= await axios.post("htpp://localhost:8000/register",{userData})
+            //     if(response.data.success){
+            //         alert("Registration successfull.")
+            //     }
+            // }catch(error){
+            //     alert(error.response.data.message)
+            // }
+
+            const myUsers= JSON.parse (localStorage.getItem("USERS")) || [];
+
+            myUsers.push(userData)
+
+            localStorage.setItem("USERS",JSON.stringify(myUsers))
+
+            alert("Registration successfull.")
+
+    } else {
+            alert("Please fill the all values.")
+    }
+}
 
     return(
         <div>
             <h1>Register</h1>
 
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Name</label><br/>
-                <input type="text" onChange={handleChange}name="name"/><br/>
+                <input type="text" onChange={handleChange}name="name" required/><br/>
                 <label>Email</label><br/>
-                <input type="email"onChange={handleChange}name="email"/><br/>
+                <input type="email"onChange={handleChange}name="email"required/><br/>
                 <label>Password</label><br/>
-                <input type="password"onChange={handleChange}name="password"/><br/>
+                <input type="password"onChange={handleChange}name="password"required/><br/>
                 <input type="Submit" value="Register"/><br/>
                 
             </form>
         </div>
     )
 }
+
 export default Register;
